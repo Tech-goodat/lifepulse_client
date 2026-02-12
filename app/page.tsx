1edb9a5a76928758ffea91bf935b87d5a759d6b9
felix kiprotich <felixkiprotich2000@gmail.com>
2026-02-12 11:22:51 +0300

    designed a fully responsive landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,55 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='flex w-full items-center justify-center flex-col'>
+      <div className='flex w-full items-center justify-center flex-col lg:grid lg:grid-cols-2 lg:gap-3'>
+        <div className='flex p-5 flex-col items-center min-w-full'>
+          <h1 className='text-4xl lg:text-5xl font-bold flex w-full dark:text-white'>
+            <span className='dark:text-[#59ffb1cc] text-[rgba(8,185,103,0.8)] mr-2'>Track</span> your habits
+          </h1>
+          <h1 className='text-4xl lg:text-5xl font-bold flex w-full dark:text-white'>
+            Improve your <span className='dark:text-[#59ffb1cc] text-[rgba(8,185,103,0.8)] ml-2'>Life</span>
+          </h1>
+          <p className='text-[13px] flex w-full mt-4 dark:text-gray-300'>
+            Because the smallest daily actions shape the strongest futures. Stay consistent. Stay accountable. Stay growing.
+          </p>
+          <div className='w-full items-center hidden md:flex gap-5 mt-4'>
+            <button className='px-5 py-2 w-30 bg-[rgba(8,185,103,0.8)] text-white rounded-md font-medium cursor-pointer hover:bg-[#59ffb1cc]/90 transition duration-300'>
+              Sign Up
+            </button>
+            <button className='px-5 w-50 py-2 border border-[rgba(8,185,103,0.8)] dark:border-white dark:text-white rounded-md font-medium cursor-pointer bg-[#59ffb1cc]/10 dark:bg-inherit transition duration-300'>
+              Log in
+            </button>
+          </div>
+        </div>
+        <div className='flex w-full items-center justify-center p-5'>
+          <Image
+            src='/hero.png'
+            alt='hero'
+            width={300}
+            height={300}
+            className='w-110 block dark:hidden h-auto object-cover'
+          />
+          <Image
+            src='/dark-hero.jpg'
+            alt='hero-dark'
+            width={200}
+            height={200}
+            className='w-110 hidden dark:block h-auto rounded-xl object-cover'
+          />
+        </div>
+        <div className='w-full items-center flex md:hidden p-5 justify-between gap-5 mt-4'>
+            <button className='px-5 py-2 w-30 bg-[rgba(8,185,103,0.8)] text-white rounded-md font-medium cursor-pointer hover:bg-[#59ffb1cc]/90 transition duration-300'>
+              Sign Up
+            </button>
+            <button className='px-5 w-50 py-2 border border-[rgba(8,185,103,0.8)] dark:border-white dark:text-white rounded-md font-medium cursor-pointer bg-[#59ffb1cc]/10 dark:bg-inherit transition duration-300'>
+              Log in
+            </button>
+          </div>
+      </div>
+    </div>
   )
 }
 
