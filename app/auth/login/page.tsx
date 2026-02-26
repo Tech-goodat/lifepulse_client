69a98451fe97c5e10c6ce4c73716b26655051bb3
felix kiprotich <felixkiprotich2000@gmail.com>
2026-02-27 01:41:35 +0300

    consumed login and signup apis

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -6,9 +6,44 @@ import { GoEyeClosed } from "react-icons/go"
 
 const LogIn = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const [userData, setUserData]=useState({
+    email:'',
+    password:''
+  })
+  const handleChange = (e) => {
+  const { name, value } = e.target;
+  setUserData(prev => ({
+    ...prev,
+    [name]: value
+  }));
+};
   const handlePasswordView = (e:React.MouseEvent<HTMLButtonElement> )=>{
     e.preventDefault();
     setShowPassword(!showPassword)
+
+  
+  }
+  const HandleLogIn=(e)=>{
+    e.preventDefault()
+     fetch('http://127.0.0.1:5555/login',{
+      method:'POST',
+      headers:{'Content-Type':'application/json'},
+      body:JSON.stringify(userData)
+    })
+
+    .then((res=>{
+      if (!res.ok){
+        throw new Error('Network response was not ok');
+      }
+      return res.json()
+    }))
+    .then((data=>{
+      console.log('Success:', data);
+      sessionStorage.setItem(
+        'token', data.access_token
+      )
+    }))
+    .catch(error=>{console.error('failed to log in', error)})
   }
   return (
     <div className='flex flex-col items-center justify-center dark:text-white text-gray-700 h-screen'>
@@ -25,16 +60,16 @@ const LogIn = () => {
         <div className='flex w-full flex-col items-center justify-center'>
           <h1 className='text-lg font-bold'>Create a <span className='text-[rgba(8,185,103,0.8)]'>LifePulse</span>  Account</h1>
           <p className='text-sm mt-2 text-gray-500 dark:text-gray-300'>Join us and start tracking your habits today!</p>
-          <form className='flex mt-5 p-3 flex-col items-center justify-center gap-5 w-full'>
-            <input className='w-full text-sm p-2 border outline-none border-gray-300 rounded-md dark:bg-inherit dark:border-gray-700' type='text' placeholder='Username' />
+          <form onSubmit={HandleLogIn} className='flex mt-5 p-3 flex-col items-center justify-center gap-5 w-full'>
+            <input name='email' value={userData.email} onChange={handleChange} className='w-full text-sm p-2 border outline-none border-gray-300 rounded-md dark:bg-inherit dark:border-gray-700' type='text' placeholder='your Email' />
             <div className='flex w-full items-center justify-between border border-gray-300 rounded-md dark:border-gray-700 px-2'>
-            <input className='w-full text-sm p-2  outline-none  dark:bg-inherit' type={showPassword ? "text" : "password"} placeholder='Create a Password' />
+            <input name='password' value={userData.password} onChange={handleChange} className='w-full text-sm p-2  outline-none  dark:bg-inherit' type={showPassword ? "text" : "password"} placeholder='Your Password' />
             <button type='button'  className='text-gray-700 cursor-pointer dark:text-gray-500' onClick={handlePasswordView}>
             {showPassword ? <GoEyeClosed /> : <FaRegEye />}
             </button>
             </div>
             <button type='submit' className='w-full flex justify-center items-center text-sm p-2 bg-[rgba(8,185,103,0.8)] text-white rounded-md font-medium cursor-pointer hover:bg-[#59ffb1cc]/90 transition duration-300'>
-              Sign Up
+              Continue Login
               
             </button>
             <p className='text-sm mt-2 text-gray-500 dark:text-gray-300'>Don't  have an account? <a href='/auth/signup' className='text-[rgba(8,185,103,0.8)] dark:text-[#59ffb1cc] cursor-pointer'>Sign Up</a></p>
--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -6,6 +6,44 @@ import { GoEyeClosed } from "react-icons/go"
 
 const SignUp = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const [userData, setUserData]=useState({
+    username:'',
+    email:'',
+    password:''
+  })
+
+  const handleChange = (e) => {
+  const { name, value } = e.target;
+  setUserData(prev => ({
+    ...prev,
+    [name]: value
+  }));
+};
+
+  const HandleSubmit=(e)=>{
+    e.preventDefault()
+    fetch('http://127.0.0.1:5555/register',{
+      method:'POST',
+      headers:{'Content-Type':'application/json'},
+      body:JSON.stringify(userData)
+    })
+
+    .then((res=>{
+      if (!res.ok){
+        throw new Error('Network response was not ok');
+      }
+      return res.json()
+    }))
+    .then((data=>{
+      console.log('Success:', data);
+      sessionStorage.setItem(
+        'token', data.access_token
+      )
+    }))
+    .catch(error=>{console.error('failed to sign up', error)})
+    
+
+  }
   const handlePasswordView = (e:React.MouseEvent<HTMLButtonElement> )=>{
     e.preventDefault();
     setShowPassword(!showPassword)
@@ -25,11 +63,11 @@ const SignUp = () => {
         <div className='flex w-full flex-col items-center justify-center'>
           <h1 className='text-lg font-bold'>Create a <span className='text-[rgba(8,185,103,0.8)]'>LifePulse</span>  Account</h1>
           <p className='text-sm mt-2 text-gray-500 dark:text-gray-300'>Join us and start tracking your habits today!</p>
-          <form className='flex mt-5 p-3 flex-col items-center justify-center gap-5 w-full'>
-            <input className='w-full text-sm p-2 border outline-none border-gray-300 rounded-md dark:bg-inherit dark:border-gray-700' type='text' placeholder='Username' />
-            <input className='w-full text-sm p-2 border outline-none border-gray-300 rounded-md dark:bg-inherit dark:border-gray-700' type='text' placeholder='Your Email' />
+          <form onSubmit={HandleSubmit} className='flex mt-5 p-3 flex-col items-center justify-center gap-5 w-full'>
+            <input name='username' value={userData.username} onChange={handleChange} className='w-full text-sm p-2 border outline-none border-gray-300 rounded-md dark:bg-inherit dark:border-gray-700' type='text' placeholder='Username' />
+            <input name='email' value={userData.email} onChange={handleChange} className='w-full text-sm p-2 border outline-none border-gray-300 rounded-md dark:bg-inherit dark:border-gray-700' type='text' placeholder='Your Email' />
             <div className='flex w-full items-center justify-between border border-gray-300 rounded-md dark:border-gray-700 px-2'>
-            <input className='w-full text-sm p-2  outline-none  dark:bg-inherit' type={showPassword ? "text" : "password"} placeholder='Create a Password' />
+            <input name='password' value={userData.password} onChange={handleChange} className='w-full text-sm p-2  outline-none  dark:bg-inherit' type={showPassword ? "text" : "password"} placeholder='Create a Password' />
             <button type='button'  className='text-gray-700 cursor-pointer dark:text-gray-500' onClick={handlePasswordView}>
             {showPassword ? <GoEyeClosed /> : <FaRegEye />}
             </button>
